3a6839d718df45dfd94d0face46e98aaac6f1942
Adão Benites <adaobjr@gmail.com>
2021-06-16 16:26:24 -0400

    fix: :fire: Lint FeedBack Page

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,33 +1,33 @@
-import React, { Component } from 'react';
-import Header from '../components/Header';
+// import React, { Component } from 'react';
+// import Header from '../components/Header';
 
-// redux
-// assertions
-export default class Feedback extends Component {
-  constructor(props) {
-    super(props);
+// // redux
+// // assertions
+// export default class Feedback extends Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = { };
-    this.msgFeedback = this.msgFeedback.bind(this);
-  }
+//     this.state = { };
+//     this.msgFeedback = this.msgFeedback.bind(this);
+//   }
 
-  msgFeedback() {
-    // const { assertions } = this.props;
-    if(assertions < 3) {
-      return (
-        <p data-testid="feedback-text">Podia ser melhor...</p>
-      );
-    }
-    return (
-      <p data-testid="feedback-text">Mandou bem!</p>
-    );
-  }
-  render() {
-    return (
-      <div>
-        <Header />
-        {this.msgFeedback()}
-      </div>
-    )
-  }
-}
+//   msgFeedback() {
+//     // const { assertions } = this.props;
+//     if(assertions < 3) {
+//       return (
+//         <p data-testid="feedback-text">Podia ser melhor...</p>
+//       );
+//     }
+//     return (
+//       <p data-testid="feedback-text">Mandou bem!</p>
+//     );
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <Header />
+//         {this.msgFeedback()}
+//       </div>
+//     )
+//   }
+// }
