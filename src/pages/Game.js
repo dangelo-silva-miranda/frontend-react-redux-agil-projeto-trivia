f7d5d89a4d53a8895f2874dc19383b269b089b53
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-06-16 16:58:21 -0300

    refactor: :fire: comment unused files

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
-import Header from '../components/Header';
+// import React, { Component } from 'react';
+// import Header from '../components/Header';
 
-
-export default class Game extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-      </div>
-    )
-  }
-}
+// export default class Game extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Header />
+//       </div>
+//     )
+//   }
+// }
