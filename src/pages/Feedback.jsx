35f5e216a7204756216bb7466440fcf17c9820ed
Letycia Barreto <letyciasbarreto@gmail.com>
2021-06-16 20:36:10 -0300

    add: header

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
-import Header from '../components/Header';
-// redux
-// assertions
-export default class Feedback extends Component {
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Feedback extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { };
+    // this.state = { };
     this.msgFeedback = this.msgFeedback.bind(this);
+    this.createAvatar = this.createAvatar.bind(this);
   }
 
   msgFeedback() {
-    // const { assertions } = this.props;
+    const { assertions } = this.props;
+
     const param = 3;
     if (assertions < param) {
       return (
@@ -23,12 +25,43 @@ export default class Feedback extends Component {
     );
   }
 
+  createAvatar() {
+    const { picture, name, score } = this.props;
+    return (
+      <div>
+        <img
+          src={ picture }
+          alt="imagem do jogador"
+          data-testid="header-profile-picture"
+        />
+        <p data-testid="header-player-name">{ name }</p>
+        <p data-testid="header-score">{ score }</p>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div>
-        <Header />
         {this.msgFeedback()}
+        {this.createAvatar()}
       </div>
     );
   }
 }
+
+const mapStateToProps = ({ player: { name, score, picture, assertions } }) => ({
+  name,
+  score,
+  picture,
+  assertions,
+});
+
+Feedback.propTypes = {
+  name: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+  picture: PropTypes.string.isRequired,
+  assertions: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps)(Feedback);
