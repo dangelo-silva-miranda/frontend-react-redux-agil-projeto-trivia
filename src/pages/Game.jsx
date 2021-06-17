23b515d76d67ce3bc3f72eb81ecdde4b7e78fce7
Adão Benites <adaobjr@gmail.com>
2021-06-17 16:34:33 -0400

    feat: :sparkles: Requirement Completed

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Timer from '../components/Timer';
+import './css/Game.css';
 
 class Game extends Component {
   constructor(props) {
@@ -10,14 +11,21 @@ class Game extends Component {
 
     this.state = {
       indexQuestion: 0,
-      // chosenAnswer: false,
+      chosenAnswer: false,
     };
     this.renderQuestions = this.renderQuestions.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      chosenAnswer: true,
+    });
   }
 
   renderQuestions() {
     const { questions } = this.props;
-    const { indexQuestion } = this.state;
+    const { indexQuestion, chosenAnswer } = this.state;
     const questionSelected = questions[indexQuestion];
     const {
       category,
@@ -25,9 +33,21 @@ class Game extends Component {
       correct_answer: correctAnswer,
       incorrect_answers: incorrectAswers } = questionSelected;
 
+    const classButtons = (index, chosenAns) => {
+      if ((index || index === 0) && chosenAns) {
+        return 'incorrectAnswer';
+      }
+      if ((!index && chosenAnswer)) {
+        return 'correctAnswer';
+      }
+      return '';
+    };
+
     const button = (answer, index) => (
       <button
         type="button"
+        onClick={ this.handleClick }
+        className={ classButtons(index, chosenAnswer) }
         data-testid={ (index || index === 0) ? (
           `wrong-answer-${index}`) : ('correct-answer') }
       >
